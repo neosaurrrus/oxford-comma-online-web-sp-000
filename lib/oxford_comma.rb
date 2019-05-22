0c69b5a1d8c2897e96e00b2cd0c9978fc42cd7fc
Lukie Kang <lukiek+f@gmail.com>
2019-05-22 21:53:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+  if array.size == 1
+    return array[0]
+  end
+  if array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  end
+  oxford_array=[]
+  array.each_with_index do |word, index|
 
+    if index+1 == array.size
+      oxford_array.push(word)
+    elsif index +2 == array.size
+      oxford_array.push(word << ", and ")
+    else
+    oxford_array.push(word << ", ")
+    end
+  end
+  oxford_array.join
 end
