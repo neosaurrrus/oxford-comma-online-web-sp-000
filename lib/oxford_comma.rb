d464c813fbf27f9927c2dc7ef7a0338ee014c948
Lukie Kang <lukiek+f@gmail.com>
2019-05-22 21:48:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,8 @@ def oxford_comma(array)
   if array.size == 1
     return array[0]
   end
+  if array.size == 2
+    return "#{array[0]} and #{array[1]}"
 
   oxford_array=[]
   array.each_with_index do |word, index|
